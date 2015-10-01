e6ffd9dd7b54eff44f1d2de59bc3a7c15865eca0
Timo Hofmeijer <t@hofmeijer.me>
2015-10-01 13:38:41 +0200

    `ts/js` sourcemaps; exclude `/node_modules` in tsconfig

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var mocha = require("gulp-mocha");
 var del = require("del");
 var listing = require("gulp-task-listing");
 var tslint = require("gulp-tslint");
+var sourcemaps = require("gulp-sourcemaps");
+var path = require("path");
 
 var configTypescript = require("./tsconfig.json").compilerOptions;
 configTypescript.typescript = require("typescript");
@@ -12,7 +14,14 @@ var tsProject = ts.createProject(configTypescript);
 
 gulp.task("build", function() {
   return gulp.src(["src/**/*.ts", "test/**/*.ts", "typings/**/*.d.ts"])
+    .pipe(sourcemaps.init())
     .pipe(ts(tsProject))
+    .pipe(sourcemaps.write(".", {
+      includeContent: false,
+      sourceRoot: function(file) {
+        return file.relative.split(path.sep).map(function(){return ".."}).join("/")+"/../";
+      }
+    }))
     .pipe(gulp.dest("build/js"));
 });
 
